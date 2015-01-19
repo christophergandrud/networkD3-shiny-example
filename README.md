02fe98865a418cc85bff46e8e8ce559a7dedd55c
christophergandrud <christopher.gandrud@gmail.com>
2015-01-19 08:47:18 +0100

    slider and screen shot

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ networkD3-shiny-example
 
 An example of a Shiny web app using networkD3 graphs.
 
-To run the example from R simply use:
+To run the example from R use:
 
 ```S
 shiny::runGitHub('christophergandrud/networkD3-shiny-example')
 ```
+
+You should then see something like this:
+
+![screen shot](ShinyScreenShot.png)
--- app.R
+++ app.R
@@ -21,17 +21,6 @@ server <- function(input, output) {
                  Group = "group", opacity = input$opacity)
   })
 
-  output$sankey <- renderSankeyNetwork({
-    library(RCurl)
-    URL <- "https://raw.githubusercontent.com/christophergandrud/d3Network/sankey/JSONdata/energy.json"
-    Energy <- getURL(URL, ssl.verifypeer = FALSE)
-    EngLinks <- JSONtoDF(jsonStr = Energy, array = "links")
-    EngNodes <- JSONtoDF(jsonStr = Energy, array = "nodes")
-    sankeyNetwork(Links = EngLinks, Nodes = EngNodes, Source = "source",
-                  Target = "target", Value = "value", NodeID = "name",
-                  fontsize = 12, nodeWidth = 30)
-  })
-
 }
 
 #### UI ####
@@ -42,14 +31,13 @@ ui <- shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      numericInput("opacity", "Opacity (not for Sankey)", 0.6, min = 0.1,
-      max = 1, step = .1)
+      sliderInput("opacity", "Opacity (not for Sankey)", 0.6, min = 0.1,
+                    max = 1, step = .1)
     ),
     mainPanel(
       tabsetPanel(
         tabPanel("Simple Network", simpleNetworkOutput("simple")),
-        tabPanel("Force Network", forceNetworkOutput("force")),
-        tabPanel("Sankey Network", sankeyNetworkOutput("sankey"))
+        tabPanel("Force Network", forceNetworkOutput("force"))
       )
     )
   )
